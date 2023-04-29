3b69a24b78d222318ce82c22f50301ee79aa8b41
Farzan-Far <farzankhalaj76@gmail.com>
2023-04-29 13:05:31 +0330

    Config the Api-GW module

--- apigw/src/main/java/com/java/farzan/ApiGWApplication.java
+++ apigw/src/main/java/com/java/farzan/ApiGWApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
-@SpringBootApplication
 @EnableEurekaClient
+@SpringBootApplication
 public class ApiGWApplication
 {
     public static void main(String[] args)

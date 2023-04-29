153180fe40c24068a8e8a42127d7e33ae7f1240b
Farzan-Far <farzankhalaj76@gmail.com>
2023-04-29 12:29:35 +0330

    Add the API-GW to the project for Load Balancing

--- apigw/src/main/java/com/java/farzan/ApiGWApplication.java
+++ apigw/src/main/java/com/java/farzan/ApiGWApplication.java
@@ -1,18 +1,16 @@
-package com.Farzan.client;
+package com.java.farzan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients(basePackages = "com.java.farzan")
-public class ClientApplication
+public class ApiGWApplication
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(ClientApplication.class,args);
+        SpringApplication.run(ApiGWApplication.class,args);
     }
 }
--- client/src/main/java/com/Farzan/client/Controller/ClientController.java
+++ client/src/main/java/com/Farzan/client/Controller/ClientController.java
@@ -7,7 +7,6 @@ import com.Farzan.client.Service.ClientServiceImpl;
 import com.sun.jdi.InternalException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
